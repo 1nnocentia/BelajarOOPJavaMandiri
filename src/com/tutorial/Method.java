4edf2e2b5273750de44be3507cf3f412090a012f
Innocentia <innocentiahan@gmail.com>
2025-02-20 15:15:49 +0800

    menambahkan method parameter di file method

--- src/com/tutorial/Method.java
+++ src/com/tutorial/Method.java
@@ -4,9 +4,14 @@ public class Method { //kalau class awalnya pakai kapital
     public static void main(String[] args) {
         //method = function
         sayHelloWorld();
+        sayHello("Eko","Khannedy");
     }
     static void sayHelloWorld(){ //kalau method kata pertamanya huruf kecil lanjutnya kapital lagi
         System.out.println("Hello World");
     
+    }
+    //method parameter
+    static void sayHello(String firstname, String lastname) {
+        System.out.println("Hello" + firstname + " " + lastname);
     }
 }
