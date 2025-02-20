1503bfa564cc0a13fd665de30155a8463ad59663
Innocentia <innocentiahan@gmail.com>
2025-02-20 15:11:47 +0800

    menambahkan method

--- src/com/tutorial/Method.java
+++ src/com/tutorial/Method.java
@@ -1,7 +1,12 @@
 package com.tutorial;
 
-public class Method {
+public class Method { //kalau class awalnya pakai kapital
     public static void main(String[] args) {
-        
+        //method = function
+        sayHelloWorld();
+    }
+    static void sayHelloWorld(){ //kalau method kata pertamanya huruf kecil lanjutnya kapital lagi
+        System.out.println("Hello World");
+    
     }
 }
